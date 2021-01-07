4ce19489d64c273594c8797386906f5d1eaa8439
charbitz <charbitz20@gmail.com>
2021-01-07 22:33:39 +0200

    Initial commit of header file (design in class form).

---
+++ assignment_6/mean_filter.h
@@ -0,0 +1,53 @@
+#ifndef MEAN_FILTER_H
+#define MEAN_FILTER_H
+
+//#include <ac_int.h>
+//#include <ac_channel.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+#include "mc_scverify.h"
+
+static const int N = 10;
+static const int M = 10;
+static const int RUNS = 10;
+
+
+typedef ac_int<8,false> pxl;
+
+using std::endl;
+
+#pragma hls_design top
+class MeanFilter
+{
+	private:
+		pxl p1, p2, p3, p4, p5, p;
+	public:
+		MeanFilter()
+		{
+			p = 0;
+		};
+
+
+#pragma hls_design interface
+	void CCS_BLOCK (mean_filter)(pxl img[N][M], pxl out[N][M])
+	{
+
+		ROW: for(int i=0;i<N;++i)
+		{
+			COL: for(int j=0;j<M;++j)
+			{
+				p1 = (j > 1)? img[i][j-2]: p;
+				p2 = (j > 0)? img[i][j-1]: p;
+				p3 = img[i][j];
+				p4 = (j < M-1)? img[i][j+1]: p ;
+				p5 = (j < M-2)? img[i][j+2]: p ;
+				out[i][j] = (p1 + p2 + p3 + p4 + p5) / 5;
+			}
+		}	
+	}
+
+};
+
+#endif
